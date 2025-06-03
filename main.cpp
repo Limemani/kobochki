4a7871d72ea1a68d6899a7fe1f6fd55b0c0bd4d1
limemani <ivan.horror2018@yandex.ru>
2025-06-03 22:42:59 +0300

    CSV file gen
    added WIP csv generator

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
+=======
+#include "csv.h"
+
+int main(int argc, char *argv[]) {
+    // Implement func call!
+    return 0;
+>>>>>>> f34005f (in the beninging)
 }
